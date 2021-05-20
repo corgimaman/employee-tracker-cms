5f4079da9733f5e3f19dc438db12060d82ed3d5d
romantic <ida.shalilian@gmail.com>
2021-05-19 23:07:00 -0500

    view is working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+const cTable = require('console.table');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -13,64 +14,80 @@ const startingPrompt = [
     {
         name: "startingPoint",
         message: "What would you like to do?",
-        choices: ["Add a department, role, or employee", "View departments, roles, or employees", "Update employee roles"],
+        choices: ["Add a department, role, or employee", "View departments, roles, or employees", "Update employee roles", "Exit App"],
         type: "rawlist"
     }
 ];
 
+connection.connect(err => {
+  if(err) throw err;
+  console.log("Welcome to the EMS!");
+  initApp();
+});
+
 // Bonus answers to add to choices: "Update employee managers", "View employees by manager", "Delete departments, roles, or employees", "View total utilized budget of a department"
 
-inquirer
-  .prompt(startingPrompt)
-  .then(answer => {
-    switch(answer.startingPoint) {
-      case 'Add a department, role, or employee':
-        addToTable();
-        break;
+function initApp() {
+  inquirer
+    .prompt(startingPrompt)
+    .then(answer => {
+      switch(answer.startingPoint) {
+        case 'Add a department, role, or employee':
+          addToTable();
+          break;
 
-      case 'View departments, roles, or employees':
-        viewTables();
-        break;
+        case 'View departments, roles, or employees':
+          viewTables();
+          break;
 
-      case 'Update employee roles':
-        updateRoles();
-        break;
-      /*
-      case 'Update employee managers':
-        updateManagers();
-        break;
+        case 'Update employee roles':
+          updateRoles();
+          break;
+        /*
+        case 'Update employee managers':
+          updateManagers();
+          break;
 
-      case 'View employees by manager':
-        viewEmployeesByManager();
-        break;
-      
-      case 'Delete departments, roles, or employees':
-        deleteRows();
-        break;
+        case 'View employees by manager':
+          viewEmployeesByManager();
+          break;
+        
+        case 'Delete departments, roles, or employees':
+          deleteRows();
+          break;
 
-      case 'View total utilized budget of a department':
-        departmentBudget();
-        break;
-      */
-      default:
-        console.log(`Invalid action: ${answer.startingPrompt}`);
-        break;  
-    }
-  })
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-      console.log(error.isTtyError);
-    } else {
-      // Something else went wrong
-      console.log(error);
-    }
-  });
+        case 'View total utilized budget of a department':
+          departmentBudget();
+          break;
+        */
+       case 'Exit App':
+         console.log("Good bye!");
+         connection.end();
+         break;
+         
+        default:
+          console.log(`Invalid action: ${answer.startingPrompt}`);
+          break;  
+      }
+    })
+    .catch(error => {
+      if(error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+        console.log(error.isTtyError);
+      } else {
+        // Something else went wrong
+        console.log(error);
+      }
+    });
+}
 
+// Create
 
 const addToTable = () => {
   console.log("add to table!");
 };
+
+// Read
 
 const viewTables = () => {
   console.log("view table!");
@@ -80,7 +97,6 @@ const viewTables = () => {
     message: 'Choose which you want to see:',
     choices: ['Departments', 'Roles', 'Employees']
   }).then(answer => {
-    console.log(answer)
     let table;
     let columns;
     switch(answer.viewTable) {
@@ -94,7 +110,7 @@ const viewTables = () => {
         break;
       case 'Employees':
         table = 'employee';
-        columns = 'first_name, last_name'
+        columns = 'first_name, last_name';
         break;
       default:
         console.log(`Invalid action: ${answer.viewTable}`);
@@ -102,12 +118,25 @@ const viewTables = () => {
     }
 
     let query = `SELECT ${columns} FROM ${table}`
-    console.log(query);
-
+    //console.log(query);
+    connection.query(query, function(err, res) {
+      if (err) throw err;
+      console.table(res);
+      // Add a timeout to let user see result before starting app again
+      setTimeout(() => initApp(), 3000);
+    });
 
   });
 };
 
+// Edit
+
 const updateRoles = () => {
   console.log("update roles!");
 };
+
+// Delete
+
+const deleteRows = () => {
+  console.log("delete rows!");
+}
